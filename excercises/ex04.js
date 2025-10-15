f910893be7bc96e4a98e3acfbddff7106457d041
hlim30-art <hlim30@ucsc.edu>
2025-10-14 18:49:39 -0700

    some

---
+++ excercises/ex04.js
@@ -0,0 +1,23 @@
+let totalNumber = 0;
+
+$("#needy-button").click ( function(){
+    totalNumber = totalNumber +3;
+
+    let sentence = "Gril wahtever ";
+    let totalNumbers= sentence + totalNumber;
+
+    $("#needy-button").html(totalNumbers);
+});
+
+
+//add a button try to clicl me <little note for human>
+
+//whn it is clicked
+
+//show how many times 
+//add one to the total number
+
+//show "cliceked total nimber times"
+
+
+//top limit
